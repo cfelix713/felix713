65f532bc423e69650ce4ab695577b1a3b0da518e
cfelix713 <60981903+cfelix713@users.noreply.github.com>
2020-02-12 14:52:06 -0300

    Add files via upload

---
+++ Consulta-serviço.ps1
@@ -0,0 +1,11 @@
+﻿#Consultar um serviço do Windows
+
+$Serv = Get-Service -Name Spooler
+If ($Serv.Status -eq "running") 
+    { 
+    Write-Host "Serviço Em execução"
+    } 
+    else
+    {
+    Write-Host "Serviço Parado"
+    }
